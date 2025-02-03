bbd7092641ddfd48619853b3fe0384c88760c1c2
Rodolfo Oliviera <rodolforomao@gmail.com>
2025-02-02 21:02:48 -0300

    Alteração da taxa

--- api.py
+++ api.py
@@ -10,12 +10,14 @@ app = FastAPI()
 async def trade(
     amount: float = Query(..., description="Valor em BRL para conversão"),
     pair: str = Query(..., description="Par de moedas (exemplo: BTCBRL, USDTBRL)"),
-    operation: str = Query(..., description="Operação desejada: 'buy' ou 'sell'")
+    operation: str = Query(..., description="Operação desejada: 'buy' ou 'sell'"),
+    tax_rate: float = Query(..., description="Taxa que será cobrada")
+    
 ):
     if operation not in ["buy", "sell"]:
         return {"error": "Operação inválida. Use 'buy' ou 'sell'."}
 
-    resultado = await trade_crypto(amount, pair, operation)
+    resultado = await trade_crypto(amount, pair, operation, tax_rate)
 
     if "error" in resultado:
         return {"error": resultado["error"]}
--- main.py
+++ main.py
@@ -14,14 +14,14 @@ async def get_binance_price(pair: str) -> float:
     
     raise Exception(f"Erro ao acessar API Binance: {response.status_code}")
 
-async def trade_crypto(amount: float, pair: str, operation: str) -> dict:
+async def trade_crypto(amount: float, pair: str, operation: str, tax_rate: float) -> dict:
     """Realiza compra ou venda de criptomoeda com base na cotação da Binance."""
     if operation not in ["buy", "sell"]:
         return {"error": "Operação inválida. Use 'buy' ou 'sell'."}
 
     try:
         price = await get_binance_price(pair)
-        tax_rate = get_trade_tax(amount)
+        #tax_rate = get_trade_tax(amount)
         fee_service_brl = (amount * tax_rate) / 100
 
         total = amount - fee_service_brl - DEPIX_TAX if operation == "buy" else amount + fee_service_brl + DEPIX_TAX
@@ -32,9 +32,6 @@ async def trade_crypto(amount: float, pair: str, operation: str) -> dict:
     except Exception as e:
         return {"error": str(e)}
 
-def get_trade_tax(amount: float) -> int:
-    """Determina a taxa de negociação com base no valor."""
-    return 7 if amount <= 9999.99 else 6 if amount < 20000 else 5
 
 # Exemplo de uso:
 # import asyncio
